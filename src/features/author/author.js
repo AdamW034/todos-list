66e67040c851c1fdd002c8421a8a7fc742e41315
Adam Wieczorek <adamwieczorek034@gmail.com>
2023-05-15 10:00:27 +0200

    author discription corrected

--- src/features/author/author.js
+++ src/features/author/author.js
@@ -8,7 +8,7 @@ const AboutAuthor = () => (
     <TextInfo>
       <p>Mam 23 lata. Pasjonują mnie muzyka, motoryzacja, oraz bron palna.<br/>
       Programowaniem zainteresowałem się około 6 miesięcy temu i tyle też się go uczę.<br/>
-      Chętnie podejmuję się satwianych przede mnią wyzwań i szybko zdobywam potzrebną wiedzę.</p>
+      Chętnie podejmuję się satwianych przede mnią wyzwań i szybko zdobywam potrzebną do zrealizowania ich wiedzę.</p>
     </TextInfo>
   </About>
 );
